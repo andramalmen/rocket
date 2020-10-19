1099d73084bd608dc41ee1ea33da305b901e603f
Andra Malmen <andra.malmen@tieto.com>
2020-10-19 23:59:06 +0300

    Add node debugger

--- rocket-api/src/__tests__/example.ts
+++ rocket-api/src/__tests__/example.ts
@@ -3,3 +3,7 @@ import { addProducts } from '../resources/product/product.controller';
 test('testing', () => {
     expect(addProducts).toBeDefined();
 });
+
+test('it works', () => {
+    expect(addProducts).toBeDefined();
+});
